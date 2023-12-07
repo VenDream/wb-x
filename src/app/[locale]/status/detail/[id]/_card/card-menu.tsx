9fde51498e70a29ac04e32dd8e0ce2daa6cf2b6f
VenDream <yeshenxue@qq.com>
2023-12-07 17:35:53 +0800

    feat(status card menu): add copy UID operation

--- src/app/[locale]/status/detail/[id]/_card/card-menu.tsx
+++ src/app/[locale]/status/detail/[id]/_card/card-menu.tsx
@@ -7,6 +7,7 @@
  * Copyright © 2023 VenDream. All Rights Reserved.
  */
 
+import useToast from '@/components/common/toast';
 import {
   Button,
   Dropdown,
@@ -17,7 +18,9 @@ import {
 import { WEIBO_HOST, WEIBO_IMAGES_DOWNLOAD_API } from '@/contants';
 import { WEIBO_ICON } from '@/contants/svgs';
 import { Link } from '@/navigation';
+import { copyText } from '@/utils/common';
 import {
+  ClipboardDocumentListIcon,
   EllipsisHorizontalCircleIcon,
   FolderArrowDownIcon,
 } from '@heroicons/react/24/outline';
@@ -34,8 +37,9 @@ const ALIGN_END_TRIGGER_W = 1450;
 
 export default function CardMenu(props: CardHeaderProps) {
   const { status, isRetweet } = props;
-  const { id } = props.status;
+  const { id, user } = props.status;
   const t = useTranslations('pages.status.menu');
+  const { showSuccessTips } = useToast();
   const [alignEnd, setAlignEnd] = useState(false);
 
   const hasImages = status.images.length > 0;
@@ -57,6 +61,8 @@ export default function CardMenu(props: CardHeaderProps) {
     };
   }, [refreshAlignment]);
 
+  if (user.id === '-1') return null;
+
   return (
     <Dropdown end={alignEnd} className={className}>
       <DropdownToggle button={false}>
@@ -65,6 +71,18 @@ export default function CardMenu(props: CardHeaderProps) {
         </Button>
       </DropdownToggle>
       <DropdownMenu className="border-regular-5 z-10 mt-2 w-[185px] rounded">
+        <DropdownItem anchor={false}>
+          <span
+            className="rounded p-2"
+            onClick={() => {
+              copyText(status.user.id);
+              showSuccessTips(t('copySuccessTips'));
+            }}
+          >
+            <ClipboardDocumentListIcon />
+            {t('copyUID')}
+          </span>
+        </DropdownItem>
         <DropdownItem anchor={false}>
           <Link
             target="_blank"
--- src/utils/common.ts
+++ src/utils/common.ts
@@ -16,3 +16,22 @@
 export function sleep(ms: number) {
   return new Promise(resolve => setTimeout(resolve, ms));
 }
+
+/**
+ * copy text
+ *
+ * @export
+ * @param {string} text text
+ */
+export function copyText(text: string) {
+  if (navigator.clipboard) {
+    navigator.clipboard.writeText(text);
+  } else {
+    const textarea = document.createElement('textarea');
+    textarea.value = text;
+    document.body.appendChild(textarea);
+    textarea.select();
+    document.execCommand('copy');
+    document.body.removeChild(textarea);
+  }
+}
