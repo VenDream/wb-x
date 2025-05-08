1e0881d314bc1b6a36dbc17257e9c0a532b765ef
VenDream <yeshenxue@qq.com>
2025-05-08 18:31:47 +0800

    feat(twitter): add twitter db stats

--- src/app/[locale]/stats.tsx
+++ src/app/[locale]/stats.tsx
@@ -12,6 +12,7 @@ import Counter from '@/components/common/counter';
 import { Stats as IStats } from '@/components/daisyui';
 import {
   BoxIcon,
+  DatabaseIcon,
   MessageSquareTextIcon,
   ShirtIcon,
   UsersIcon,
@@ -44,28 +45,55 @@ export default async function Stats() {
   const t = await getTranslations('pages.home');
 
   const { size = '0MB', tables } = dbInfo || {};
-  const { wb_users = 0, wb_statuses = 0, rotn_items = 0 } = tables || {};
+  const {
+    wb_users = 0,
+    wb_statuses = 0,
+    twitter_users = 0,
+    twitter_tweets = 0,
+    rotn_items = 0,
+  } = tables || {};
   const fileSizeNum = +size.split('MB')[0] || 0;
 
-  const statUnits: StatUnitProps[] = [
+  const dbStatUnits: StatUnitProps[] = [
     {
       title: t('totalDbSize'),
       value: fileSizeNum,
       desc: 'MB',
       icon: <BoxIcon size={24} className="text-accent" />,
     },
+  ];
+
+  const wbStatUnits: StatUnitProps[] = [
     {
-      title: t('totalUsers'),
+      title: t('totalWeiboUsers'),
       value: wb_users,
       desc: t('records'),
       icon: <UsersIcon size={24} className="text-accent" />,
     },
     {
-      title: t('totalStatuses'),
+      title: t('totalWeiboStatuses'),
       value: wb_statuses,
       desc: t('records'),
       icon: <MessageSquareTextIcon className="text-accent" />,
     },
+  ];
+
+  const twitterStatUnits: StatUnitProps[] = [
+    {
+      title: t('totalTwitterUsers'),
+      value: twitter_users,
+      desc: t('records'),
+      icon: <UsersIcon size={24} className="text-accent" />,
+    },
+    {
+      title: t('totalTwitterTweets'),
+      value: twitter_tweets,
+      desc: t('records'),
+      icon: <MessageSquareTextIcon className="text-accent" />,
+    },
+  ];
+
+  const rotnStatUnits: StatUnitProps[] = [
     {
       title: t('totalRotns'),
       value: rotn_items,
@@ -75,12 +103,52 @@ export default async function Stats() {
   ];
 
   return (
-    <div>
+    <div className="space-y-4">
+      <h1 className="flex items-center text-2xl">
+        <DatabaseIcon size={24} className="mr-2" />
+        {t('dbTitle')}
+      </h1>
       <IStats
-        direction="vertical"
+        direction="horizontal"
         className="border-base-content/10 bg-base-200 border"
       >
-        {statUnits.map((unit, idx) => (
+        {dbStatUnits.map((unit, idx) => (
+          <StatUnit key={idx} {...unit} />
+        ))}
+      </IStats>
+      <h1 className="flex items-center text-2xl">
+        <UsersIcon size={24} className="mr-2" />
+        {t('wbTitle')}
+      </h1>
+      <IStats
+        direction="horizontal"
+        className="border-base-content/10 bg-base-200 border"
+      >
+        {wbStatUnits.map((unit, idx) => (
+          <StatUnit key={idx} {...unit} />
+        ))}
+      </IStats>
+      <h1 className="flex items-center text-2xl">
+        <UsersIcon size={24} className="mr-2" />
+        {t('twitterTitle')}
+      </h1>
+      <IStats
+        direction="horizontal"
+        className="border-base-content/10 bg-base-200 border"
+      >
+        {twitterStatUnits.map((unit, idx) => (
+          <StatUnit key={idx} {...unit} />
+        ))}
+      </IStats>
+      <h1 className="flex items-center text-2xl">
+        <ShirtIcon size={24} className="mr-2" />
+        {t('rotnTitle')}
+      </h1>
+      <IStats
+        direction="horizontal"
+        className="border-base-content/10 bg-base-200 border"
+      >
+        {rotnStatUnits.map((unit, idx) => (
           <StatUnit key={idx} {...unit} />
         ))}
       </IStats>
---
+++ src/types/twitter.d.ts
@@ -0,0 +1,173 @@
+/*
+ * Twitter Types
+ *
+ * @Author: VenDream
+ * @Date: 2025-05-08 16:36:09
+ *
+ * Copyright © 2025 VenDream. All Rights Reserved.
+ */
+
+namespace Twitter {
+  /* ------------------------------------------------------------------------ */
+  /*                                   User                                   */
+  /* ------------------------------------------------------------------------ */
+  interface User {
+    /** user id */
+    id: string;
+    /** `username@screen_name` */
+    name: string;
+    /** description */
+    desc: string;
+    /** link */
+    link: string;
+    /** avatar */
+    avatar: string;
+    /** banner image */
+    banner: string;
+    /** location */
+    location: string;
+    /** `username@screen_name` */
+    screenName: string;
+    /** created at */
+    createdAt: string;
+    /** media count */
+    mediaCount: number;
+    /** tweets count */
+    tweetsCount: number;
+    /** follow count */
+    followCount: number;
+    /** followers count */
+    followersCount: number;
+    /** is tracking */
+    isTracking: boolean;
+  }
+
+  /* ------------------------------------------------------------------------ */
+  /*                                   Tweet                                  */
+  /* ------------------------------------------------------------------------ */
+  interface Tweet {
+    /** tweet id */
+    id: string;
+
+    /** author */
+    user: User;
+    /** text */
+    text: string;
+    /** raw text */
+    rawText: string;
+    /** created at */
+    createdAt: string;
+    /** source */
+    source: string;
+    /** images */
+    images: string[];
+    /** images count */
+    imagesCount: number;
+    /** videos */
+    videos: Video[];
+    /** videos count */
+    videosCount: number;
+    /** quoted tweet */
+    quotedTweet: Tweet | null;
+    /** retweeted tweet */
+    retweetedTweet: Tweet | null;
+
+    /** view count */
+    viewCount: number;
+    /** reposts count */
+    repostsCount: number;
+    /** comments count */
+    commentsCount: number;
+    /** bookmark count */
+    bookmarkCount: number;
+    /** favorite count */
+    favoriteCount: number;
+
+    /** is original */
+    isOriginal: boolean;
+    /** is favourite */
+    isFavourite: boolean;
+    /** has videos */
+    hasVideos: boolean;
+    /** has images */
+    hasImages: boolean;
+  }
+
+  interface Video {
+    /** video id */
+    id: string;
+    /** video url */
+    url: string;
+    /** video cover url */
+    cover: string;
+    /** video duration */
+    duration: number;
+    /** original video aspect ratio */
+    aspectRatio: [number, number];
+  }
+
+  /* ------------------------------------------------------------------------ */
+  /*                                  Params                                  */
+  /* ------------------------------------------------------------------------ */
+  interface UserListFilterParams {
+    /** id */
+    id?: string;
+    /** keyword */
+    keyword?: string;
+    /** is tracking */
+    isTracking?: boolean;
+  }
+
+  interface TweetsListFilterParams {
+    /** tweet id */
+    id?: string;
+    /** tweet uid */
+    uid?: string;
+    /** fav uid */
+    favUid?: string;
+    /** is tracking */
+    isTracking?: boolean;
+    /** is original */
+    isOriginal?: boolean;
+    /** is favourite */
+    isFavourite?: boolean;
+    /** has videos */
+    hasVideos?: boolean;
+    /** has images */
+    hasImages?: boolean;
+    /** start date */
+    startDate?: string;
+    /** end date */
+    endDate?: string;
+    /** least images count */
+    leastImagesCount?: number;
+    /** least videos count */
+    leastVideosCount?: number;
+  }
+
+  interface ScannerOption {
+    /** uid */
+    uid: string;
+    /** cursor */
+    cursor?: string;
+
+    /** scan all or not */
+    all?: boolean;
+    /** upload or not */
+    upload?: boolean;
+    /** force upload or not */
+    forceUpload?: boolean;
+
+    /** days to scan */
+    days?: number;
+    /** start date */
+    startDate?: string;
+    /** end date */
+    endDate?: string;
+
+    /** trigger only */
+    triggerOnly?: boolean;
+    /** all tracking uids */
+    allTrackingUids?: boolean;
+  }
+}
